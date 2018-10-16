02c65726c7ed2c15a55ac2496cdc32dd2a4ad825
nadeeraka <nimanthanadeeraka@outlook.com>
2018-10-16 15:04:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+class App extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {options:['one','two','three'],
+    title:'Indection App',
+    subTitle:'shaparing your decitions'
+  };
+  }
+  render()
+  {
+    return(
+      <div>
+      <Header title={this.state.title} subTitle={this.state.subTitle}/>
+      <Action options={this.state.options} hasOption={this.state.options.length>0}/>
+      <AddOption options={this.state.options}/>
+      </div>);
   }
 }
 
+let Header = (props)=>
+{
+  return(<div>
+        <h1>{props.title}</h1>
+        <h2>{props.subTitle}</h2>
+
+        </div>)
+}
+
+class Action extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.handelButton = this.handelButton.bind(this);
+  }
+  handelButton()
+  {
+    let number = Math.floor(Math.random()*this.props.options.length);
+    let op = this.props.options[number];
+    alert(op);
+  }
+  render()
+  {
+    return(<div>
+      < button disabled={!this.props.hasOption}
+      onClick={this.handelButton}
+       > What shoud i do < /button>
+      </div>)
+  }
+}
+class AddOption extends Component
+{
+  render()
+  {
+    return(<div>
+                 <input/>
+                 <button>Add options</button>
+                  {this.props.options.map((num)=>{return <AllOPtions key={num} textValue={num} / >})}                 
+          </div>)
+  }
+}
+
+class AllOPtions extends Component
+{
+  render()
+  {
+    return(<div>
+           <li>{this.props.textValue}</li>
+          </div>)
+  }
+}
 export default App;
