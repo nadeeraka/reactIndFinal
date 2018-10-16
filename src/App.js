0052ca3243384067daa63e22b264d5f8f7eb554c
nadeeraka <nimanthanadeeraka@outlook.com>
2018-10-16 16:26:04 +0530

    add reset button

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ class App extends Component
       <div>
       <Header title={this.state.title} subTitle={this.state.subTitle}/>
       <Action options={this.state.options} hasOption={this.state.options.length>0}/>
+      < RemoveOption options = {this.state.options}/>
       <AddOption options={this.state.options}/>
+     
       </div>);
   }
 }
@@ -55,12 +57,22 @@ class Action extends Component
 }
 class AddOption extends Component
 {
+  constructor(props)
+  {
+    super(props);
+    this.handelInput = this.handelInput.bind(this);
+  }
+  handelInput()
+  {
+
+  }
   render()
   {
-    return(<div>
-                 <input/>
+    return(<div> <form onSubmit={this.handleInput}>
+                 <input />
                  <button>Add options</button>
                   {this.props.options.map((num)=>{return <AllOPtions key={num} textValue={num} / >})}                 
+                  </form>
           </div>)
   }
 }
@@ -73,5 +85,24 @@ class AllOPtions extends Component
            <li>{this.props.textValue}</li>
           </div>)
   }
+}
+class RemoveOption extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.handelRemove = this.handelRemove.bind(this);
+
+  }
+  handelRemove()
+  {
+    alert('click');
+  }
+ render()
+ {
+   return(<div>
+                <button onClick={this.handelRemove}>Reset</button>
+         </div>);
+ }
 }
 export default App;
